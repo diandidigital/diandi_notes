11e40035cfce7fde9893f25a2e31687657b9c9f7
BitmagicGit <nioule4@outlook.com>
2026-05-08 22:01:58 +0000

    chore: rename app branding

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:diandi_notes/main.dart';
+import 'package:noteflow/main.dart';
 
 void main() {
   testWidgets('renders notes home', (WidgetTester tester) async {
     await tester.pumpWidget(const DiandiNotesApp());
     await tester.pumpAndSettle();
 
-    expect(find.text('Diandi Notes'), findsOneWidget);
+    expect(find.text('NoteFlow'), findsOneWidget);
     expect(find.text('Nouvelle note'), findsOneWidget);
   });
 }
